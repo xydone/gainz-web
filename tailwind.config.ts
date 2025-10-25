7b25a303fb53b87311e174e4269b39fca73df3a4
xydone <xydone@outlook.com>
2025-10-25 20:24:56 +0300

    fix: background color treated as transparent

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,7 +10,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        background: "hsl(var(--background))",
+        background: "var(--background)",
         foreground: "var(--foreground)",
         text: {
           DEFAULT: "var(--text-color)",
